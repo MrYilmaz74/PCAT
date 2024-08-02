ef7ec8329625f373728178ae622fcf296c6dac1d
Furkan <yilmazfrkn74@gmail.com>
2024-08-02 10:13:07 +0300

    feedback

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express= require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.get('/', (req,res) =>{
-  const photo ={
-    id:1,
-    name:"Photo Name",
-    description:"Photo description"
-  }
-  res.send(photo)
-})
+// MIDDLEWARES
+app.use(express.static('public'));
 
-const port =3000
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı.`)
-})
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
